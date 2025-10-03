cfe22318339066da878666b6ecbb6dac40907517
Joaquin Peralta <jperaltac@gmail.com>
2025-10-03 11:19:04 -0300

    Fix formatting of API umbrella header

--- liblpmd/lpmd/api.h
+++ liblpmd/lpmd/api.h
@@ -13,71 +13,45 @@
 
 /**
  *  \mainpage LPMD: Las Palmeras Molecular Dynamics
-#include <lpmd/error.h>
-#include <lpmd/physunits.h>
-#include <lpmd/timer.h>
-#include <lpmd/util.h>
-// Plugin loading
-
-// Math
-#include <lpmd/vector.h>
-// Simulation data
-#include <lpmd/atompair.h>
-#include <lpmd/pairpotential.h>
-#include <lpmd/potential.h>
-#include <lpmd/potentialarray.h>
-// Molecular dynamics
-
-#include <lpmd/temporalproperty.h>
-
-#include <lpmd/plugin.h>
-#include <lpmd/pluginmanager.h>
-
-// Math 
-
-#include <lpmd/vector.h>
-#include <lpmd/matrix.h>
-#include <lpmd/scalartable.h>
-#include <lpmd/scalarvalue.h>
-#include <lpmd/vectorvalue.h>
-
-// Simulation data 
+ *
+ *  This header aggregates the most commonly used headers in the LPMD
+ *  codebase so that applications can include the complete public API with a
+ *  single directive.
+ */
 
 #include <lpmd/atom.h>
-#include <lpmd/cell.h>
-#include <lpmd/particles.h>
 #include <lpmd/atompair.h>
-#include <lpmd/simulationcell.h>
-
-// Potentials
-
-#include <lpmd/potential.h>
-#include <lpmd/potentialarray.h>
-#include <lpmd/coulomb.h>
-#include <lpmd/pairpotential.h>
-#include <lpmd/metalpotential.h>
-
-// Integrators
-
-#include <lpmd/integrator.h>
-#include <lpmd/onestepintegrator.h>
-#include <lpmd/twostepintegrator.h>
-
-// Molecular dynamics 
-
-#include <lpmd/simulation.h>
-
-// Other plugin types (besides Potential and Integrator)
-
+#include <lpmd/cell.h>
 #include <lpmd/cellformat.h>
 #include <lpmd/cellgenerator.h>
 #include <lpmd/cellmanager.h>
 #include <lpmd/cellreader.h>
 #include <lpmd/cellwriter.h>
+#include <lpmd/coulomb.h>
+#include <lpmd/error.h>
 #include <lpmd/instantproperty.h>
-#include <lpmd/temporalproperty.h>
+#include <lpmd/integrator.h>
+#include <lpmd/matrix.h>
+#include <lpmd/metalpotential.h>
+#include <lpmd/onestepintegrator.h>
+#include <lpmd/pairpotential.h>
+#include <lpmd/particles.h>
+#include <lpmd/physunits.h>
+#include <lpmd/plugin.h>
+#include <lpmd/pluginmanager.h>
+#include <lpmd/potential.h>
+#include <lpmd/potentialarray.h>
+#include <lpmd/scalartable.h>
+#include <lpmd/scalarvalue.h>
+#include <lpmd/simulation.h>
+#include <lpmd/simulationcell.h>
 #include <lpmd/systemmodifier.h>
+#include <lpmd/temporalproperty.h>
+#include <lpmd/timer.h>
+#include <lpmd/twostepintegrator.h>
+#include <lpmd/util.h>
+#include <lpmd/vector.h>
+#include <lpmd/vectorvalue.h>
 #include <lpmd/visualizer.h>
 
 #endif
-
